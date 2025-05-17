e7c022a5194090a2411a771a3c8ef6b8e97a4b2e
MangeshMScalar123 <m.mangeshmankar@gmail.com>
2025-05-17 09:49:12 +0530

    New EMartProductService created

---
+++ src/main/java/com/emart/emartproductservice/EMartProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.emart.emartproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMartProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMartProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emart/emartproductservice/EMartProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emart.emartproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMartProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
